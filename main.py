191000e8bd556937418c02a96da8837b1a303a60
mqk233 <mqk233@outlook.com>
2023-07-17 10:38:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,17 @@ if __name__ == '__main__':
     zip_files.close()
     with open(file='ssr-master/Clash.yml', mode='r', encoding='utf-8') as clash_file:
         clash_content = yaml.load(clash_file, Loader=yaml.FullLoader)
+        proxies = clash_content['proxies']
+        remove_index = -1
+        for i in range(len(proxies)):
+            if proxies[i]['name'].find('公益机场') != -1:
+                remove_index = i
+                break
+        if remove_index != -1:
+            proxies.pop(remove_index)
     os.mkdir(path='myfiles')
     with open(file='myfiles/MyClash.yml', mode='w', encoding='utf-8') as result_clash_file:
-        yaml.dump(data={'mixed-port': 7890, 'mode': 'global', 'proxies': clash_content['proxies']},
+        yaml.dump(data={'mixed-port': 7890, 'mode': 'global', 'proxies': proxies},
                   stream=result_clash_file,
                   allow_unicode=True,
                   sort_keys=False)
