b085c38e3772568442cd6ef812b64600ac1eb764
mqk233 <mqk233@outlook.com>
2024-01-27 16:12:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,12 @@ if __name__ == '__main__':
     with open(file='Clash.yml', mode='r', encoding='utf-8') as clash_file:
         lines_to_keep = []
         for line in clash_file:
-            if '!<str>' not in line:
+            if '!<str>' not in line or '公益机场' not in line:
                 lines_to_keep.append(line)
         clash_content = yaml.safe_load(''.join(lines_to_keep))
-        proxies = clash_content['proxies']
-        remove_index = -1
-        for i in range(len(proxies)):
-            if proxies[i]['name'].find('公益机场') != -1:
-                remove_index = i
-                break
-        if remove_index != -1:
-            proxies.pop(remove_index)
     os.mkdir(path='myfiles')
     with open(file='myfiles/MyClash.yml', mode='w', encoding='utf-8') as result_clash_file:
-        yaml.dump(data={'mixed-port': 7890, 'mode': 'global', 'proxies': proxies},
+        yaml.dump(data={'mixed-port': 7890, 'mode': 'global', 'proxies': clash_content['proxies']},
                   stream=result_clash_file,
                   allow_unicode=True,
                   sort_keys=False)
