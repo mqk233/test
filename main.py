22fe1ff7430342788510ebce6080bd839b87b001
mqk233 <mqk233@outlook.com>
2024-01-27 15:57:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ import wget
 import yaml
 
 if __name__ == '__main__':
-    wget.download(url="https://github.com/ssrsub/ssr/archive/refs/heads/master.zip", out='ssr-master.zip')
-    zip_files = zipfile.ZipFile(file='ssr-master.zip')
-    for zip_file in zip_files.namelist():
-        zip_files.extract(zip_file)
-    zip_files.close()
-    with open(file='ssr-master/Clash.yml', mode='r', encoding='utf-8') as clash_file:
+    wget.download(url="https://github.com/ssrsub/ssr/raw/master/Clash.yml", out='Clash.yml')
+    with open(file='Clash.yml', mode='r', encoding='utf-8') as clash_file:
         clash_content = yaml.safe_load(clash_file)
         proxies = clash_content['proxies']
         remove_index = -1
