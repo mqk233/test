a88106088804b56d3ae05ca5aff79e74682802fa
mqk233 <mqk233@outlook.com>
2024-01-27 16:13:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with open(file='Clash.yml', mode='r', encoding='utf-8') as clash_file:
         lines_to_keep = []
         for line in clash_file:
-            if '!<str>' not in line or '公益机场' not in line:
+            if '!<str>' not in line and '公益机场' not in line:
                 lines_to_keep.append(line)
         clash_content = yaml.safe_load(''.join(lines_to_keep))
     os.mkdir(path='myfiles')
